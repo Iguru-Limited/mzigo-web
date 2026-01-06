c8616e1988512d7770e4b45a8e1dfa740a28435d
1-ken <kennicmwimu@gmail.com>
2026-01-06 21:57:12 +0300

    ....

--- app/(main)/load/page.tsx
+++ app/(main)/load/page.tsx
@@ -7,7 +7,7 @@ export default function LoadPage() {
   return (
     <div className="flex flex-1 flex-col gap-4 p-4 md:gap-6 md:p-6">
       <div className="flex items-center justify-between">
-        <h1 className="text-2xl font-bold md:text-3xl">Loading</h1>
+        {/* <h1 className="text-2xl font-bold md:text-3xl">Loading</h1> */}
       </div>
 
       <div className="grid gap-4 md:gap-6">
